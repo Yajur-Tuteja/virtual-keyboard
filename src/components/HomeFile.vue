d00427ffccb7fb508d3f6b69860e88723b30443f
Yajur Tuteja <69846355+Yajur-Tuteja@users.noreply.github.com>
2022-08-13 18:55:06 +0530

    Update HomeFile.vue

--- src/components/HomeFile.vue
+++ src/components/HomeFile.vue
@@ -1,12 +1,15 @@
 <template>
 <div class="keyboard">
-  <div class="a" v-for="line in vals.users" :key="line">
-     <div class="b" v-for="ch in line.chars" :key="ch">
-         <button id="c" v-if="vals.tog && line.type!='special keys'" v-on:click="write(ch.toUpperCase());shuffle(vals.users)">{{ch.toUpperCase()}}</button>
-         <button id="c" v-else v-on:click="write(ch);shuffle(vals.users)">{{ch}}</button>
+  <div class="a" v-for="kind in vals.users" :key="kind">
+     <div class="b" v-for="ch in kind.chars" :key="ch"> 
+        <button id="k" v-if="vals.isTab && ch=='back'" v-on:click="vals.isTab=false;write('backtab')">{{ch}}</button>
+        <button id="k" v-else-if="vals.tog && kind.type!='special keys'" v-on:click="write(ch.toUpperCase());shuffle(vals.users)">{{ch.toUpperCase()}}</button>
+        <button id="k" v-else v-on:click="write(ch);shuffle(vals.users)">{{ch}}</button>
      </div> 
   </div>
+  
   <button v-on:click="vals.tog=!vals.tog">caps</button>
+  <button v-on:click="write('tab');vals.isTab=true">tab</button>
 </div>
 </template>
 
@@ -14,32 +17,31 @@
 export default {
   name: 'HomeFile',
   props:{
-    data:String,
-    msg:String,
-    val:String 
-  },data(){
+    data:String
+  },
+  data(){
       return{ 
         vals:{users:[
             {type:'numbers',chars:['1','2','3','4','5','6','7','8','9','0']},
-            {row:'letters',chars:['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']},
-            {type:'sp',chars:['!','@','#','~','`','$','%','^','&','*','(',')','-','_','|','?',',','.','<','>',':','{','[',']','=','+','/',';']},
+            {type:'letters',chars:['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']},
+            {type:'symbols',chars:['!','@','#','~','`','$','%','^','&','*','(',')','-','_','|','?',',','.','<','>',':','{','[',']','=','+','/',';']},
             {type:'special keys',chars:['back','clear','   space   ']}
-       ],tog:false}
+       ],tog:false,isTab:false}
       }
     },
-    methods:{
-      write(value){
-          this.$emit("key-is-pressed",value);
-      },
-      shuffle(keys){
-        for(let i=0;i<keys.length-1;i++){
-           for(let j=keys[i].chars.length-1;j>0;j--){
-              const k=Math.floor(Math.random()*(j+1));
-              const temp=keys[i].chars[j];
-              keys[i].chars[j]=keys[i].chars[k];
-              keys[i].chars[k]=temp;
-            }
+methods:{
+  write(value){
+      this.$emit("key-is-pressed",value);
+    },
+   shuffle(keys){
+      for(let i=0;i<keys.length-1;i++){
+        for(let j=keys[i].chars.length-1;j>0;j--){
+            const k=Math.floor(Math.random()*(j+1));
+            const temp=keys[i].chars[j];
+            keys[i].chars[j]=keys[i].chars[k];
+            keys[i].chars[k]=temp;
           }
+        }
       }
     }
 }
@@ -49,8 +51,6 @@ export default {
 <style>
   .a{
     display: flex;
-    flex-wrap: wrap;
     justify-content: left;
   }
-
 </style>
